7dcb61df52831b96adf515ceab1e683197315bd1
Franziska Seitz <58744794+loerk@users.noreply.github.com>
2020-10-22 10:50:11 +0200

    counter
    counter and background

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const daysEl= document.getElementById('days');
+const hoursEl= document.getElementById('hours');
+const minsEl= document.getElementById('minutes');
+const secondsEl= document.getElementById('seconds');
+
+
+const Birthday = "02 Nov 2020";
+
+function countdown(){
+    const BirthdayDate = new Date(Birthday);
+    const currentDate = new Date();
+    
+    const totalSeconds = new Date(BirthdayDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds/3600)%24;
+    const mins = Math.floor(totalSeconds/ 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+        
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+//adds zero to seconds
+function formatTime(time) {
+    return time < 10? `0${time}` : time;
+}
+// initial call
+countdown();
+setInterval(countdown, 1000);
