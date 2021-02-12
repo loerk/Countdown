c7ef6b95f6033503f977490372e9da6daeafc8d2
Fraser <58744794+loerk@users.noreply.github.com>
2021-02-12 17:28:05 +0100

    update birthday

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const minsEl= document.getElementById('minutes');
 const secondsEl= document.getElementById('seconds');
 
 
-const Birthday = "01 Jan 2021";
+const Birthday = "01 Jan 2022";
 
 function countdown(){
     const BirthdayDate = new Date(Birthday);
